7d02baf95274fc038b5add134d6ab632b60d35d2
Lucas Justice <35172162+JusticeLuke@users.noreply.github.com>
2022-02-28 16:52:51 -0500

    updated images
    Changed some images to jpg, and added smaller sizes to improve load time

--- Projects/ArtPage/ArtPageScripts/get_art.js
+++ Projects/ArtPage/ArtPageScripts/get_art.js
@@ -2,7 +2,8 @@ function getName(name) {
   var titleName = document.getElementById(name).getAttribute("id");
   document.getElementById("exampleModalLabel").innerHTML = titleName;
 
-  var imgSrc = document.getElementById(name).getAttribute("src");
+  var imgSrc = document.getElementById(name).getAttribute("srcset");
+  console.log(imgSrc);
   var img = document.createElement("IMG");
   img.setAttribute("src",imgSrc);
   img.setAttribute("class","modalArt img-fluid")
